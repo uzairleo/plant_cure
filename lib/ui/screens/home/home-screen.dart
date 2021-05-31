b1d5d0179610b50d519f50466d5611dbb25456a7
uzairleo <uzair.jan336@gmail.com>
2021-05-31 10:48:50 +0500

    homescreen-ui

--- lib/ui/screens/home/home-screen.dart
+++ lib/ui/screens/home/home-screen.dart
@@ -1,19 +1,57 @@
 import 'package:farmer_assistant_app/core/constants/colors.dart';
+import 'package:farmer_assistant_app/core/models/crop.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final List<Crop> addedCrops;
+  HomeScreen({this.addedCrops});
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      backgroundColor: backgroundColor,
+            backgroundColor: backgroundColor,
 
-      ///
-      ///[body] start from here
-      ///
-      body: Center(
-        child: Text("home..."),
-      ),
-    ));
+            ///
+            ///[body] start from here
+            ///
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  //weather and notification section cart with setting popup menu button as well
+                  weatherAndNotifications(),
+
+                  //added fruit list in horizntal view with edit button iconbutton
+                  addedFruits(),
+
+                  //fertilizer and pest cart with tiles
+                  fertilizerAndPests(),
+
+                  //check health card having check health button
+                  checkHealthButton(),
+                ],
+              ),
+            )));
+  }
+
+  //weather and notification section cart with setting popup menu button as well
+  weatherAndNotifications() {
+    return Container();
+  }
+
+  //added fruit list in horizntal view with edit button iconbutton
+  addedFruits() {
+    return Container();
+  }
+
+  //fertilizer and pest cart with tiles
+  fertilizerAndPests() {
+    return Container();
+  }
+
+  //check health card having check health button
+  checkHealthButton() {
+    return Container();
   }
 }
